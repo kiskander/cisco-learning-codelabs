ff9b13d22dd24d28de9c0ab767b60929115de9c2
kiskander <kiskander@users.noreply.github.com>
2022-03-10 22:35:50 +0000

    published new content

--- docs/google_codelab_bin.js
+++ docs/google_codelab_bin.js
@@ -932,11 +932,11 @@
     function sc(a) {
         a = a.ga;
         return R(
-            '<div id="drawer">Drawer</div><div id="main"><div id="codelab-title"><div id="codelab-nav-buttons"><a href="' +
+            '<div id="main"><div id="codelab-title"><div id="codelab-nav-buttons"><a href="' +
                 hc(mc(a)) +
                 '" id="arrow-back"><i class="material-icons">close</i></a><a href="#" id="menu"><i class="material-icons">menu</i></a></div><div id="codelab-time-container"></div><devsite-user></devsite-user></div><div id="steps"></div><div id="controls"><div id="fabs"><a href="#" id="previous-step" title="Previous step">Back</a><div class="spacer"></div><a href="#" id="next-step" title="Next step">Next</a><a href="' +
                 hc(mc(a)) +
-                '" id="done" hidden title="Codelab complete">Done</a></div></div></div>'
+                '" id="done" hidden title="Codelab complete">Done</a></div></div></div><div id="drawer">Drawer</div>'
         );
     }
     function tc(a) {
